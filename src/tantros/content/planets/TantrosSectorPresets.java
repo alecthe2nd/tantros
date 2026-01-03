10858e622b606772a7d8ad50c3cfbd83a33a3d52
Alecthe2nd <alecthe2nd@outlook.com>
2026-01-02 22:28:16 -0600

    Updated various bundle records updated Embark added Old Reef

--- src/tantros/content/planets/TantrosSectorPresets.java
+++ src/tantros/content/planets/TantrosSectorPresets.java
@@ -5,12 +5,18 @@ import mindustry.type.SectorPreset;
 import static mindustry.content.Planets.tantros;
 
 public class TantrosSectorPresets {
-    public static SectorPreset embark;
+    public static SectorPreset embark,
+    oldReef;
 
     public static void load(){
         embark = new SectorPreset("embark", tantros, 10){{
             alwaysUnlocked = true;
             addStartingItems = true;
+            captureWave = 10;
+            difficulty = 1;
+            overrideLaunchDefaults = true;
+        }};
+        oldReef = new SectorPreset("old-reef", tantros, 83){{
             captureWave = 10;
             difficulty = 1;
             overrideLaunchDefaults = true;
