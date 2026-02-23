5550a3f50689f54c2b9cc582b0e96c5a3bfeb369
Alecthe2nd <alecthe2nd@outlook.com>
2026-02-22 21:52:38 -0600

    Added Individual Planet Clearing dialog

--- src/tantros/Tantros.java
+++ src/tantros/Tantros.java
@@ -1,7 +1,26 @@
 package tantros;
 
+import arc.Core;
+import arc.files.Fi;
+import arc.scene.ui.TextButton;
+import arc.scene.ui.layout.Table;
+import arc.struct.ObjectMap;
+import arc.util.Log;
+import arc.util.Strings;
 import mindustry.Vars;
+import mindustry.Vars.*;
+import mindustry.content.Planets;
+import mindustry.content.TechTree;
+import mindustry.core.GameState;
+import mindustry.ctype.UnlockableContent;
+import mindustry.game.Saves;
+import mindustry.gen.Icon;
+import mindustry.gen.Tex;
 import mindustry.mod.*;
+import mindustry.type.Planet;
+import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
+import mindustry.ui.dialogs.SettingsMenuDialog;
 import tantros.ai.TantrosUnitCommands;
 import tantros.content.blocks.*;
 import tantros.content.planets.*;
@@ -9,10 +28,19 @@ import tantros.content.world.*;
 import tantros.gen.*;
 import tantros.content.recipes.TantrosRecipes;
 import tantros.net.TantrosCalls;
+import tantros.ui.ClearPlanetsDialog;
+
+import java.lang.reflect.Field;
+
+import static arc.Core.app;
+import static arc.Core.settings;
+import static mindustry.Vars.*;
 
 public class Tantros extends Mod{
 
     public static float waterToSteamConversion = 2f;
+
+    public static ClearPlanetsDialog clearPlanetsDialog;
 
     @Override
     public void loadContent(){
@@ -32,7 +60,27 @@ public class Tantros extends Mod{
     @Override
     public void init() {
         super.init();
-        Vars.ui.settings.graphics.checkPref("fast-parallax", Vars.mobile);
-        Vars.ui.settings.graphics.checkPref("drill-assist-indicators", false);
+        ui.settings.graphics.checkPref("fast-parallax", Vars.mobile);
+        ui.settings.graphics.checkPref("drill-assist-indicators", false);
+
+        Log.info("Attempting to access data settings ui.");
+        clearPlanetsDialog = new ClearPlanetsDialog();
+        try {
+            Field field = SettingsMenuDialog.class.getDeclaredField("dataDialog");
+            field.setAccessible(true);
+            BaseDialog dataDialog = (BaseDialog) field.get(ui.settings);
+            dataDialog.cont.row();
+            dataDialog.cont.table(Tex.button, t -> {
+                t.defaults().size(280f, 60f).left();
+                t.button(
+                        "@setting.clear-specific-planet",
+                        ()->{
+                            clearPlanetsDialog.show();
+                        }
+                ).marginLeft(4);
+            });
+        } catch (Exception e){
+            Log.err(e);
+        }
     }
 }
---
+++ src/tantros/ui/ClearPlanetsDialog.java
@@ -0,0 +1,65 @@
+package tantros.ui;
+
+import arc.Core;
+import arc.scene.ui.layout.Table;
+import mindustry.content.TechTree;
+import mindustry.ctype.UnlockableContent;
+import mindustry.gen.Tex;
+import mindustry.type.Planet;
+import mindustry.ui.dialogs.BaseDialog;
+
+import static arc.Core.bundle;
+import static mindustry.Vars.*;
+
+public class ClearPlanetsDialog extends BaseDialog {
+
+    public ClearPlanetsDialog() {
+        super(bundle.get("setting.clear-specific-planet", "Clear Specific Planet"));
+        addCloseButton();
+
+
+        this.cont.table(Tex.button, t -> {
+            t.defaults().size(280f, 60f).left();
+            for(Planet planet: content.planets()){
+                if(planet.accessible){
+                    addButton(t, planet);
+                }
+                t.row();
+            }
+        });
+    }
+
+
+    public void addButton(Table t, Planet planet){
+
+        t.button(
+                Core.bundle.format("setting.clear-planet", planet.localizedName),
+                ()->{
+
+                    ui.showConfirm("@confirm", Core.bundle.format("setting.clear-planet.confirm", planet.localizedName), () -> {
+                        for (var sec : planet.sectors) {
+                            sec.clearInfo();
+                            if (sec.save != null) {
+                                sec.save.delete();
+                                sec.save = null;
+                            }
+                        }
+
+                        for (var slot : control.saves.getSaveSlots().copy()) {
+                            if (slot.isSector() && slot.meta.rules != null && slot.meta.rules.planet == planet) {
+                                slot.delete();
+                            }
+                        }
+                        //universe.clearLoadoutInfo();
+
+                        planet.techTree.each(TechTree.TechNode::reset);
+                        content.each(c -> {
+                            if(c instanceof UnlockableContent u && u.isOnPlanet(planet)){
+                                u.clearUnlock();
+                            }
+                        });
+                    });
+                }
+        ).marginLeft(4);
+    }
+}
