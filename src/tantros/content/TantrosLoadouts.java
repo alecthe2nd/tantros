587ede48b0dc137f14d4ade28f7ae83edd04971b
Alecthe2nd <alecthe2nd@outlook.com>
2026-02-24 21:53:37 -0600

    Added code that injects my loadouts into the loadout cache through the vanilla approved method

--- src/tantros/content/TantrosLoadouts.java
+++ src/tantros/content/TantrosLoadouts.java
@@ -1,13 +1,31 @@
 package tantros.content;
 
+import arc.util.Log;
+import mindustry.Vars;
 import mindustry.game.Schematic;
 import mindustry.game.Schematics;
+
+import java.lang.reflect.Method;
 
 public class TantrosLoadouts {
 
     public static Schematic basicShell;
 
     public static void load(){
-        basicShell = Schematics.readBase64("bXNjaAF4nGNgYWBhZmDJS8xNZeByzi9KtQrOSM3JYeBOSS1OLsosKMnMz2NgYGDLSUxKzSlmYIqOZWQQKknMKynKL9ZNBqrXLQarZ2BgBCEgAQBy2hSU");
+        basicShell = build("bXNjaAF4nGNgYWBhZmDJS8xNZeByzi9KtQrOSM3JYeBOSS1OLsosKMnMz2NgYGDLSUxKzSlmYIqOZWQQKknMKynKL9ZNBqrXLQarZ2BgBCEgAQBy2hSU");
+    }
+
+    public static Schematic build(String schemeString){
+        Schematic schematic = Schematics.readBase64(schemeString);
+
+        try{
+            Method m = Schematics.class.getDeclaredMethod("checkLoadout", Schematic.class, boolean.class);
+            m.setAccessible(true);
+            m.invoke(Vars.schematics, schematic, false);
+        } catch (Exception e) {
+            Log.err(e);
+        }
+
+        return schematic;
     }
 }
